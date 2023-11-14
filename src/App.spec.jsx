6773bd350c8e0511e52dc7f3f415c08f4ef16752
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-14 12:41:58 -0300

    [cfg] - base structure

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,16 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import App from "./App";
 import { describe, expect, it } from "vitest";
 
 describe("App", () => {
-  it("renders headline", () => {
+  it("should render component", () => {
     const components = render(<App />);
-    /*test para verificar que el componente tiene un texto en específico, en este caso "react" en modo no estricto */
-    const heading = components.getByRole("heading", {name: /react/i,}).textContent;
 
-    /*test para verificar que el componente tiene cualquier texto*/
-    // const heading = screen.getByRole("heading");
-    // expect(heading.textContent).toBeTruthy();
+    /*test para verificar que el componente existe*/
+    expect(components).toBeDefined;
+
+    // test para verificar que el componente tiene un texto en específico, en este caso "react" en modo no estricto
+    /*const heading = components.getByRole("heading", {
+      name: /react/i,
+    }).textContent; */
+
+    // test para verificar que el componente tiene cualquier texto
+    /*  const heading = screen.getByRole("heading");
+    expect(heading.textContent).toBeTruthy(); */
   });
 });
