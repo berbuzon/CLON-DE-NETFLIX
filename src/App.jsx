41010032482eefa3eba8c6c2b72e49fa6af27678
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-17 11:56:54 -0300

    [cfg] add pipeline QA

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 const App = () => {
-  return <div>
-    <h1>Hola Celina</h1>;
-    <h2>TESTING version 2</h2>
-  </div>
+  return (
+    <div>
+      <h1>Hola Celina</h1>;<h2>TESTING version 3</h2>
+    </div>
+  );
 };
 
 export default App;
