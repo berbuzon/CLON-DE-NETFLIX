a6be7a70242bf15c32b6789897c561ea7473a617
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-17 22:19:07 -0300

    [cfg] - environments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { environment } from "@environments";
+// import { environment } from "@environments";
 
 const App = () => {
   return (
     <div>
       <h1>ESTO ES PARA QUE EL DESARROLLADOR TESTEE</h1>
-      <h2>{environment.title}</h2>
+      {/* <h2>{environment.title}</h2> */}
     </div>
   );
 };
