5e235890bb60aac5bdb41cffd8da529e0b9a2105
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-21 15:57:09 -0300

    [cfg] - base structure

--- src/core/routes/app_router.jsx
+++ src/core/routes/app_router.jsx
@@ -3,9 +3,7 @@ import HomeView from "../../features/home/views/home_view";
 import LoginView from "../../features/login/views/login_view";
 
 export const appRouter = createBrowserRouter([
-  { path: "/", element: 
-  <PrivateRoute>
-    <HomeView />
-  </PrivateRoute> },
+  { path: "/", element:
+  <HomeView /> },
   { path: "/login", element: <LoginView /> },
 ]);
