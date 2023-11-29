f29cb74ac39867eb800afb6fde581f66e5cb8915
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-29 20:11:09 -0300

    tmdb

--- src/features/home/views/home_view.jsx
+++ src/features/home/views/home_view.jsx
@@ -6,12 +6,13 @@ import "./home_view.css";
 import axios from "axios";
 
 const TMDB_API = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  api_key: "dcd601b15c6ef59ab712cd1686a133ea",
-  params: { language: "es-ES" },
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
+    language: "es-ES",
+  },
 });
 
-console.log(TMDB_API);
 console.log(import.meta.env.VITE_TMDB_API_KEY);
 
 const HomeView = () => {
