1c82122f729dabccc8b69b54e9a5aaf3af73f4d1
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-29 18:17:19 -0300

    tmdb

--- src/features/home/views/home_view.jsx
+++ src/features/home/views/home_view.jsx
@@ -3,6 +3,16 @@ import { useAuth } from "../../../core/auth/hooks/useAuth";
 import { authApi } from "../../../core/datasource/remote/auth/auth_api";
 import "./home_view.css";
 
+import axios from "axios";
+
+const TMDB_API = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  api_key: import.meta.env.VITE_APP_TMDB_API_KEY,
+  params: { language: "es-ES" },
+});
+
+console.log(import.meta.env.VITE_APP_TMDB_API_KEY);
+
 const HomeView = () => {
   const [user, setUser] = useState(null);
   const { logout } = useAuth();
