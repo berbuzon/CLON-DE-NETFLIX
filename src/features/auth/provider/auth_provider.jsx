a9b458aed01e309d26acb23c07b1626b1feca4e2
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-30 08:25:03 -0300

    Comments

--- src/features/auth/provider/auth_provider.jsx
+++ src/features/auth/provider/auth_provider.jsx
@@ -5,14 +5,25 @@ import { authApi } from "../../../core/datasource/remote/auth/auth_api";
 export const AUTH_KEY = "isLoggedIn";
 
 export const AuthProvider = ({ children, fallback }) => {
+  // isLoggedIn indica si el usuario está logueado o no.
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  // isLoading indica si se está cargando algo.
   const [isLoading, setIsLoading] = useState(true);
 
+  // setItem guarda el estado de isLoggedIn en el localStorage.
   const saveLoginState = (state) => AppStorage.setItem(AUTH_KEY, state);
 
+  // getItem obtiene el estado de isLoggedIn del localStorage.
   const getLoginState = () => AppStorage.getItem(AUTH_KEY);
 
+  // removeItem elimina el estado de isLoggedIn del localStorage.
   const removeItem = () => AppStorage.removeItem(AUTH_KEY);
+
+  // initAuth obtiene el estado de inicio de sesión del localStorage.
+  // si es false, no hace nada.
+  // si es true, setea el estado de isLoggedIn en true.
+  // finalmente, setea el estado de isLoading en false que indica que ya no se está cargando nada.
 
   useEffect(() => {
     const initAuth = async () => {
@@ -31,20 +42,19 @@ export const AuthProvider = ({ children, fallback }) => {
     initAuth();
   }, []);
 
-  const login = async (email, password) => {
-
-    // esto es para enviar datos al servidor
-    /* await authApi.post("/login", { email, password }); */
-    /*console.log(response.data); */
-
+  // El usuario está logueado si isLoggedIn es true.
+  // saveLoginState guarda el estado de isLoggedIn en el localStorage.
+  const login = async () => {
     setIsLoggedIn(true);
     saveLoginState(true);
   };
+
+  // El usuario está deslogueado si isLoggedIn es false.
+  // removeItem elimina el estado de isLoggedIn del localStorage.
   const logout = async () => {
     setIsLoggedIn(false);
     removeItem();
   };
-
 
   // INTERCEPTOR DE PETICIONES HTTP (REQUEST Y RESPONSE)
   useEffect(() => {
@@ -71,17 +81,16 @@ export const AuthProvider = ({ children, fallback }) => {
     );
   });
 
-  // fallback es un componente que se muestra cuando isLoading es true. 
+  // fallback es un componente que se muestra cuando isLoading es true.
   // Podría ser, por ejemplo, un componente de carga o un mensaje de "Cargando...".
   // en este caso muestra un mensaje de Cargando...
   // si fallback es null o undefined, AuthProvider no renderiza nada.
   // En este caso siempre es true porque se lo paso por props al AuthProvider en el RootProvider
-  
-  // isLoading es una variable booleana que indica si se está cargando algo. 
+
+  // isLoading indica si se está cargando algo.
   // si isLoading es false, AuthProvider renderiza children.
 
   if (fallback && isLoading) return fallback;
-
 
   return (
     <AuthContext.Provider
