d0eb6049d15499ed5a6fe2158922ee8f775f576a
Bernardo Eliezer <berbuzon@gmail.com>
2023-11-30 17:51:42 -0300

    tmdb

--- src/features/home/services/movies.services.js
+++ src/features/home/services/movies.services.js
@@ -3,13 +3,34 @@ import {
   tmdb_paths,
 } from "../../../core/datasource/remote/tmdb/tmdb_api";
 
+//adaptador de datos de la API de TMDB (The Movie Database)
+export const tmdbMoviesTvAdapter = (response) => {
+  const { results } = response;
+  return results.map((item) => {
+    return {
+      id: item.id,
+      title: item.title || item.name,
+      poster: `${tmdb_paths.images.poster.url}
+            ${tmdb_paths.images.poster.sizes.original}
+            ${item.poster_path}`,
+      backdrop: `${tmdb_paths.images.backdrop.url}
+            ${tmdb_paths.images.backdrop.sizes.original}
+            ${item.backdrop_path}`,
+      description: item.overview,
+      rating: item.vote_average,
+      video: item.video,
+    };
+  });
+};
+
 //hace una solicitud GET a la API de TMDB (The Movie Database)
 //para obtener las películas populares
 
 export const getPopularMovies = async () => {
-  const {data} = await tmdb_api.get(tmdb_paths.movies.popular);
+  const { data } = await tmdb_api.get(tmdb_paths.movies.popular);
 
-  console.log("data", data);
+  const adapted = tmdbMoviesTvAdapter(data);
+  console.log("data", adapted);
 
-  return data;
+  return adapted;
 };
